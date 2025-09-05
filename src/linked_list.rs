15efb19b535198cce78df62f2b6863868c1ff4e3
Mark Sadiki <msadiki06@icloud.com>
2025-09-05 11:06:38 -0500

    Add traits and iterators to LinkedList implementation

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,5 +1,6 @@
 use std::mem;
 
+#[derive(Default)]
 pub struct LinkedList<T> {
     first: *mut LinkedNode<T>,
     last: *mut LinkedNode<T>,
@@ -54,7 +55,7 @@ impl <T> LinkedList<T> {
         }
 
         self.length += 1;
-        self.last  = raw;
+        self.last = raw;
         if self.first.is_null() {
             self.first = raw;
         }
@@ -138,10 +139,338 @@ impl<T> Drop for LinkedList<T> {
     }
 }
 
+impl<T: Clone> Clone for LinkedList<T> {
+    fn clone(&self) -> Self {
+        let mut new_list = LinkedList::new();
+
+        // If empty, return an empty list
+        if self.first.is_null() {
+            return new_list;
+        }
+
+        // Iterate through nodes and clone each one
+        let mut current = self.first;
+        while !current.is_null() {
+            let node = unsafe { &*current };
+            new_list.push_back(node.inner.clone());
+            current = node.next;
+        }
+
+        new_list
+    }
+}
+
+impl<T: std::fmt::Debug> std::fmt::Debug for LinkedList<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut list = f.debug_list();
+
+        let mut current = self.first;
+        while !current.is_null() {
+            let node = unsafe { &*current };
+            list.entry(&node.inner);
+            current = node.next;
+        }
+
+        list.finish()
+    }
+}
+
+impl<T: PartialEq> PartialEq for LinkedList<T> {
+    fn eq(&self, other: &Self) -> bool {
+        if self.length != other.length {
+            return false;
+        }
+
+        let mut self_current = self.first;
+        let mut other_current = other.first;
+
+        while !self_current.is_null() && !other_current.is_null() {
+            let self_node = unsafe { &*self_current };
+            let other_node = unsafe { &*other_current };
+
+            if self_node.inner != other_node.inner {
+                return false;
+            }
+
+            self_current = self_node.next;
+            other_current = other_node.next;
+        }
+
+        self_current.is_null() && other_current.is_null()
+    }
+}
+
+impl<T: Eq> Eq for LinkedList<T> {}
+
+impl<T: PartialOrd> PartialOrd for LinkedList<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        let mut self_current = self.first;
+        let mut other_current = other.first;
+
+        while !self_current.is_null() && !other_current.is_null() {
+            let self_node = unsafe { &*self_current };
+            let other_node = unsafe { &*other_current };
+
+            match self_node.inner.partial_cmp(&other_node.inner) {
+                Some(std::cmp::Ordering::Equal) => {
+                    self_current = self_node.next;
+                    other_current = other_node.next;
+                }
+                non_equal => return non_equal,
+            }
+        }
+
+        // If we've exhausted one list but not the other, the shorter is "less"
+        if self_current.is_null() && other_current.is_null() {
+            Some(std::cmp::Ordering::Equal)
+        } else if self_current.is_null() {
+            Some(std::cmp::Ordering::Less)
+        } else {
+            Some(std::cmp::Ordering::Greater)
+        }
+    }
+}
+
+impl<T: Ord> Ord for LinkedList<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        let mut self_current = self.first;
+        let mut other_current = other.first;
+
+        while !self_current.is_null() && !other_current.is_null() {
+            let self_node = unsafe { &*self_current };
+            let other_node = unsafe { &*other_current };
+
+            match self_node.inner.cmp(&other_node.inner) {
+                std::cmp::Ordering::Equal => {
+                    self_current = self_node.next;
+                    other_current = other_node.next;
+                }
+                non_equal => return non_equal,
+            }
+        }
+
+        // If we've exhausted one list but not the other, the shorter is "less"
+        if self_current.is_null() && other_current.is_null() {
+            std::cmp::Ordering::Equal
+        } else if self_current.is_null() {
+            std::cmp::Ordering::Less
+        } else {
+            std::cmp::Ordering::Greater
+        }
+    }
+}
+
+impl<T, const N: usize> From<[T; N]> for LinkedList<T> {
+    fn from(arr: [T; N]) -> Self {
+        Self::from_iter(arr)
+    }
+}
+
+impl<T> FromIterator<T> for LinkedList<T> {
+    fn from_iter<I: IntoIterator<Item=T>>(iter: I) -> Self {
+        let mut list = LinkedList::new();
+        for item in iter {
+            list.push_back(item);
+        }
+        list
+    }
+}
+
+// Explicitly implement Send for LinkedList<T> when T is Send
+unsafe impl<T: Send> Send for LinkedList<T> {}
+
+// Explicitly implement Sync for LinkedList<T> when T is Sync
+unsafe impl<T: Sync> Sync for LinkedList<T> {}
+
+// Iterator types and methods for LinkedList
+impl<T> LinkedList<T> {
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            next: self.first,
+            phantom: std::marker::PhantomData,
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            next: self.first,
+            phantom: std::marker::PhantomData,
+        }
+    }
+}
+
+// Immutable iterator for LinkedList
+pub struct Iter<'a, T> {
+    next: *mut LinkedNode<T>,
+    phantom: std::marker::PhantomData<&'a T>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next.is_null() {
+            None
+        } else {
+            // Safety: next is non-null and comes from a valid LinkedList
+            let node = unsafe { &*self.next };
+            self.next = node.next;
+            // Safety: the lifetime of the reference is tied to the lifetime of the LinkedList
+            Some(unsafe { &*(&node.inner as *const T) })
+        }
+    }
+}
+
+// Mutable iterator for LinkedList
+pub struct IterMut<'a, T> {
+    next: *mut LinkedNode<T>,
+    phantom: std::marker::PhantomData<&'a mut T>,
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next.is_null() {
+            None
+        } else {
+            // Safety: next is non-null and comes from a valid LinkedList
+            let node = unsafe { &mut *self.next };
+            self.next = node.next;
+            // Safety: the lifetime of the reference is tied to the lifetime of the LinkedList
+            Some(unsafe { &mut *(&mut node.inner as *mut T) })
+        }
+    }
+}
+
+// Consuming iterator for LinkedList
+pub struct IntoIter<T> {
+    list: LinkedList<T>,
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.list.pop_front()
+    }
+}
+
+impl<'a, T> IntoIterator for &'a LinkedList<T> {
+    type Item = &'a T;
+    type IntoIter = Iter<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<'a, T> IntoIterator for &'a mut LinkedList<T> {
+    type Item = &'a mut T;
+    type IntoIter = IterMut<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter_mut()
+    }
+}
+
+impl<T> IntoIterator for LinkedList<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter { list: self }
+    }
+}
+
+impl<T> Extend<T> for LinkedList<T> {
+    fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
+        for item in iter {
+            self.push_back(item);
+        }
+    }
+}
+
+impl<'a, T: Clone + 'a> Extend<&'a T> for LinkedList<T> {
+    fn extend<I: IntoIterator<Item = &'a T>>(&mut self, iter: I) {
+        for item in iter {
+            self.push_back(item.clone());
+        }
+    }
+}
+
+impl<T: std::hash::Hash> std::hash::Hash for LinkedList<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.length.hash(state);
+
+        // Hash each element in order
+        let mut current = self.first;
+        while !current.is_null() {
+            let node = unsafe { &*current };
+            node.inner.hash(state);
+            current = node.next;
+        }
+    }
+}
+
 pub struct LinkedNode<T> {
     inner: T,
     next: *mut LinkedNode<T>,
     prev: *mut LinkedNode<T>,
+}
+
+impl<T: Clone> Clone for LinkedNode<T> {
+    fn clone(&self) -> Self {
+        LinkedNode {
+            inner: self.inner.clone(),
+            next: self.next,
+            prev: self.prev,
+        }
+    }
+}
+
+impl<T: std::fmt::Debug> std::fmt::Debug for LinkedNode<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("LinkedNode")
+            .field("inner", &self.inner)
+            .field("next", &(self.next as usize))
+            .field("prev", &(self.prev as usize))
+            .finish()
+    }
+}
+
+impl<T: PartialEq> PartialEq for LinkedNode<T> {
+    fn eq(&self, other: &Self) -> bool {
+        // Only compare the values, not the pointers
+        self.inner == other.inner
+    }
+}
+
+impl<T: Eq> Eq for LinkedNode<T> {}
+
+impl<T: PartialOrd> PartialOrd for LinkedNode<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.inner.partial_cmp(&other.inner)
+    }
+}
+
+impl<T: Ord> Ord for LinkedNode<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.inner.cmp(&other.inner)
+    }
+}
+
+// Explicitly implement Send for LinkedNode<T> when T is Send
+unsafe impl<T: Send> Send for LinkedNode<T> {}
+
+// Explicitly implement Sync for LinkedNode<T> when T is Sync
+unsafe impl<T: Sync> Sync for LinkedNode<T> {}
+
+impl<T: std::hash::Hash> std::hash::Hash for LinkedNode<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        // Hash only the inner value, consistent with PartialEq implementation
+        self.inner.hash(state);
+    }
 }
 
 
@@ -291,6 +620,3 @@ mod tests {
         assert_eq!(list.pop_back(), Some(40));
     }
 }
-
-
-
